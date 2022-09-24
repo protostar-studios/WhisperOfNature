37cf7add011237c301ed6abc906e70267d49c268
James Chen <shaohong.chen@mail.utoronto.ca>
2022-09-25 00:41:00 +0800

    delete unnecessary files

---
+++ Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int curWeather = 0;
+    public List<string> weathers = new List<string>(){"summer", "winter"};
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void updateWeather(){
+        curWeather
+    }
+}
