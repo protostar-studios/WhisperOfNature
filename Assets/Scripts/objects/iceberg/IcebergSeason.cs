e9d1ec84e09443a84d7a443797d4f7af06f6d82a
Shaohong Chen <shaohong.chen@mail.utoronto.ca>
2022-09-27 20:34:33 -0400

    add icebergh

--- Assets/Scripts/objects/iceberg/IcebergSeason.cs
+++ Assets/Scripts/objects/iceberg/IcebergSeason.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class treeFreeze : MonoBehaviour
+public class IcebergSeason : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
