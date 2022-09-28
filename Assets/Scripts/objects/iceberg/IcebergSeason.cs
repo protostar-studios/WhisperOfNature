1847aa0b47b0e9e9635353db4f60fcbaf0399ef0
Shaohong Chen <shaohong.chen@mail.utoronto.ca>
2022-09-27 21:32:11 -0400

    add iceberg1

--- Assets/Scripts/objects/iceberg/IcebergSeason.cs
+++ Assets/Scripts/objects/iceberg/IcebergSeason.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class IcebergSeason : MonoBehaviour
 {
+    private SeasonManager seasonManager;
+    private Vector3 initPos;
+    public float speed = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        seasonManager = Object.FindObjectOfType<SeasonManager>();
+        initPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (seasonManager.curWeather)
+        {
+            case 1:
+                Vector3 newPos = initPos + Vector3.up * 1;
+                transform.position = Vector3.MoveTowards(transform.position, newPos, speed * Time.deltaTime);
+                break;
+            default:
+                transform.position = Vector3.MoveTowards(transform.position, initPos, speed * Time.deltaTime);
+                break;
+        }
     }
 }
