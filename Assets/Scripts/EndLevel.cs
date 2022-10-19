6162f24978118df25fda23b2e2c055bd1148bb34
lucabaiocchi <lucabaiocchi22official@gmail.com>
2022-10-19 12:35:30 -0400

    Created end of Level checkpoint that sends back to the main level

--- Assets/Scripts/EndLevel.cs
+++ Assets/Scripts/EndLevel.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class LevelChange : MonoBehaviour
+public class EndLevel : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -15,11 +14,5 @@ public class LevelChange : MonoBehaviour
     void Update()
     {
         
-    }
-
-    private void OnCollisionEnter(Collision other) {
-        if(other.gameObject.CompareTag("Player")){
-            SceneManager.LoadScene("MainMenu");
-        }
     }
 }
