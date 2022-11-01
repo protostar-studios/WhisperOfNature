88ef7ec3586bc4ab5f48471b89e8154350d2b14c
lucabaiocchi <lucabaiocchi22official@gmail.com>
2022-11-01 13:00:30 -0400

    Created the Prefabs for Tutorials Messages, their animations, their trigger interaction for the player to remove them, their materials, related scripts

---
+++ Assets/Scripts/DestroyTutorial.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.ShaderData;
+
+public class DestroyTutorial : MonoBehaviour
+{
+
+    //Credit for the Animation Event Function to fade out the object to IndieGameDev
+    // on StackOverflow
+    // (https://stackoverflow.com/questions/65212015/how-do-you-fade-out-an-object-in-a-scene-from-an-animation-clip#:~:text=You%20can%20do%20that%20with,will%20fade%20out%20the%20object.)
+    private float secondsToDestroy = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Destroy(gameObject, secondsToDestroy);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
