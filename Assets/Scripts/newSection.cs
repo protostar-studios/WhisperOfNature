1fa96bca6b96bfdedd5dcee3dbb0550f544e6332
JamesChenS <shaohong.chen@mail.utoronto.ca>
2022-11-16 12:59:22 -0500

    fix problem that gems are reduced for opened doors

--- Assets/Scripts/newSection.cs
+++ Assets/Scripts/newSection.cs
@@ -8,6 +8,7 @@ public class newSection : MonoBehaviour
     public GameObject currGate;
     public int respawnPosIndex = 0;
     private PlayerManager playerManager;
+    private bool isopened = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +19,7 @@ public class newSection : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (playerManager.inventory["Gem"] > 0) {
+            if (playerManager.inventory["Gem"] > 0 && !isopened) {
                 // Destroy(transform.root.gameObject);
                 Destroy(currGate);
                 // Destroy Rotating Gem Indicator
