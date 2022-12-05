54f762313bbd9cf3c329050828a454bde6c695b9
Varun Lodaya <varunblodaya@gmail.com>
2022-12-05 17:50:43 -0500

    added bird chirping to scene. volume might need to be tuned

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -7,10 +7,12 @@ public class BirdScript : MonoBehaviour
     // Start is called before the first frame update
     private SeasonManager seasonManager;
     private MeshRenderer rend;
+    private AudioSource audioSource;
     void Start()
     {
         seasonManager = Object.FindObjectOfType<SeasonManager>();
         rend = GetComponent<MeshRenderer>();
+        audioSource = GetComponent<AudioSource>();
         rend.enabled = true;
                 
     }
@@ -21,9 +23,11 @@ public class BirdScript : MonoBehaviour
          if (seasonManager.curSeason != 0)
         {
             rend.enabled = false;
+            audioSource.volume = 0.0f;
         }
         else{
             rend.enabled = true;
+            audioSource.volume = 1.0f;
         }
         
     }
