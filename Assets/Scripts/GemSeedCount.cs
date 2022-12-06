ac6bec4a771fe05547002a50a806dfee727eb588
JamesChenS <shaohong.chen@mail.utoronto.ca>
2022-12-06 11:49:02 -0500

    fix GUI level 1 level 2

--- Assets/Scripts/GemSeedCount.cs
+++ Assets/Scripts/GemSeedCount.cs
@@ -14,7 +14,7 @@ public class GemSeedCount : MonoBehaviour
     //Start is called before the first frame update
     void Start()
     {
-        
+        playerManager = FindObjectOfType<PlayerManager>();
     }
 
     // Update is called once per frame
